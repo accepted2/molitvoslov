5c8581861ecc340f6fcae62033bcb31b17865d72
accepted2 <144708202+accepted2@users.noreply.github.com>
2026-09-23 16:28:58 +0300

    ui: reduce reader padding

--- molitvoslov-app/src/components/reader/SelectableDocumentReader.js
+++ molitvoslov-app/src/components/reader/SelectableDocumentReader.js
@@ -72,11 +72,11 @@ const HTML_TEMPLATE = String.raw`
     }
 
     body {
-      padding: 20px 20px 96px;
+      padding: 14px 14px 88px;
     }
 
     .rule-title {
-      margin: 0 0 24px;
+      margin: 0 0 18px;
       text-align: center;
       font-size: 26px;
       line-height: 32px;
@@ -84,7 +84,7 @@ const HTML_TEMPLATE = String.raw`
     }
 
     .rule-description {
-      margin: -8px 0 24px;
+      margin: -5px 0 18px;
       color: var(--secondary);
       font-size: 14px;
       line-height: 21px;
@@ -93,8 +93,8 @@ const HTML_TEMPLATE = String.raw`
 
     .rule-item {
       position: relative;
-      margin-bottom: 18px;
-      padding-bottom: 18px;
+      margin-bottom: 14px;
+      padding-bottom: 14px;
       border-bottom: 1px solid var(--border);
     }
 
