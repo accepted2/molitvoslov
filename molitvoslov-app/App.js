88667517cdc0cb0645e84b0c0ccd0d57983862b0
accepted2 <144708202+accepted2@users.noreply.github.com>
2026-09-23 13:10:34 +0300

    fix: use safe area provider

--- molitvoslov-app/App.js
+++ molitvoslov-app/App.js
@@ -1,16 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {AppNavigator} from "./src/navigation/AppNavigator";
+import React from 'react';
+
+import {
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+
+import {
+  AppNavigator,
+} from './src/navigation/AppNavigator';
+
 
 export default function App() {
-  return <AppNavigator/>
+  return (
+    <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
