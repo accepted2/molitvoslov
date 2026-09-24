6737d59d0cc47b290fc5220f44500fee145b0642
accepted2 <144708202+accepted2@users.noreply.github.com>
2026-09-24 17:34:02 +0300

    Remove development API token logging

--- molitvoslov-app/src/api/index.js
+++ molitvoslov-app/src/api/index.js
@@ -1,27 +1,11 @@
-import axios from 'axios'
-import { Platform } from 'react-native';
-
+import axios from 'axios';
 
 const API_URL = 'http://192.168.1.103:8000/api/';
 
-const DEV_TOKEN = process.env.EXPO_PUBLIC_DEV_TOKEN;
-
 export const api = axios.create({
-    baseURL: API_URL,
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization:`Token ${DEV_TOKEN}`
-    }
-})
-
-api.interceptors.request.use(
-  config => {
-      console.log(
-        'API REQUEST:',
-        config.url,
-        config.headers?.Authorization
-      )
-      return config
-  }
-)
+  baseURL: API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
